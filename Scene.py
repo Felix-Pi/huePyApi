f2c177facae6b3a6a96f30a43165a8569a25809c
felix-pi <felixpieschka@gmail.com>
2020-12-06 17:53:16 +0100

    added scene

--- Scene.py
+++ Scene.py
@@ -1,7 +1,13 @@
 class Scene:
-    def __init__(self, id, name, group, lights, lightstates):
-        self.id = id
+    def __init__(self, hue, scene_id, name, group, lights, lightstates):
+        self.hue = hue
+        self.scene_id = scene_id
         self.name = name
-        self.on = group
-        self.bri = lights
-        self.ct = lightstates
+        self.group = group
+        self.lights = lights
+        self.lightstates = lightstates
+
+    def __repr__(self):
+        return 'Scene(id={}, name={}, group={}, lights={}, lightstates={})'.format(self.scene_id, self.name, self.group,
+                                                                                   self.lights,
+                                                                                   self.lightstates)
